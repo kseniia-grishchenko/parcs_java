c21e2e9044dedbdb91b4b7d936df797b65190ffc
kseniia <kseniia@smartiot.cloud>
2024-04-28 17:13:45 +0200

    Distribute execution equally

--- src/SoloveySum.java
+++ src/SoloveySum.java
@@ -75,16 +75,25 @@ public class SoloveySum implements AM {
         Node n = (Node) info.parent.readObject();
         System.out.println("[" + n.l + " " + n.r + "] Build started.");
 
+        int totalNumbers = n.r - n.l + 1;
+        int numbersPerNode = totalNumbers / NODES;
+        int extra = totalNumbers % NODES;
+
+        int start = n.l + n.div * numbersPerNode + Math.min(n.div, extra);
+        int end = start + numbersPerNode - 1;
+        if (n.div < extra) {
+            end += 1;
+        }
+
         long sum = 0L;
-        int start = n.l;
-        while (start % NODES != n.div) start++;
-        for (int i = start; i <= n.r; i += NODES) {
-            long x = new Long(i);
+        for (int i = start; i <= end; i++) {
+            long x = i; // Direct use of 'i', 'new Long(i)' is unnecessary
             if(isPrime(x, 10)) {
                 System.out.println(x + " is Prime");
-                sum += i;
+                sum += x;
             }
         }
+
         System.out.println("[" + n.l + " " + n.r + "] Build finished.");
         info.parent.write(sum);
     }
